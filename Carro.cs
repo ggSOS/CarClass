a615f3d7a6b02a3c08f94e969056cb1464a05906
GustavoG <126207720+ggSOS@users.noreply.github.com>
2025-09-29 09:19:42 -0300

    Project Update

---
+++ Carro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    internal class Carro
+    {
+        private String Chassis {  get; set; }
+        private String LicensePlate { get; set; }
+        private String Cor { get; set; }
+        private String Motor { get; set; }
+
+        public Carro(string chassis, string licensePlate, string cor, string motor)
+        {
+            Chassis = chassis;
+            LicensePlate = licensePlate;
+            Cor = cor;
+            Motor = motor;
+        }
+
+        public override string ToString()
+        {
+            string output = "\t- Chassis: " + Chassis +
+                "\n\t- LicensePlate: " + LicensePlate +
+                "\n\t- Cor: " + Cor +
+                "\n\t- Motor: " + Motor;
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using CarClass;
+using System.Collections.Generic;
+
+namespace Ambiente1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Carro> listaDeCarros = new List<Carro>();
+
+            Carro carro1 = new Carro("Uno", "asd2b345", "Vermelho", "Combustao");
+            listaDeCarros.Add(carro1);
+            Carro carro2 = new Carro("Audi", "oiu6s789", "Cinza", "Combustao");
+            listaDeCarros.Add(carro2);
+            Carro carro3 = new Carro("Dodge Ram", "xcv7g234", "Preto", "Combustao");
+            listaDeCarros.Add(carro3);
+            Carro carro4 = new Carro("Hylux", "fgh5q456", "Branco", "Combustao");
+            listaDeCarros.Add(carro4);
+            Carro carro5 = new Carro("BYD", "cft6d346", "Azul", "Elétrico");
+            listaDeCarros.Add(carro5);
+            Carro carro6 = new Carro("Palio", "dhf3t789", "Branco", "Combustao");
+            listaDeCarros.Add(carro6);
+            Carro carro7 = new Carro("Corsa", "sdb6t143", "Cinza", "Combustao");
+            listaDeCarros.Add(carro7);
+            Carro carro8 = new Carro("Kwid", "sbd3h521", "Vermelho", "Combustao");
+            listaDeCarros.Add(carro8);
+            Carro carro9 = new Carro("Fusca", "bsr4u745", "Aul", "Fe");
+            listaDeCarros.Add(carro9);
+            Carro carro10 = new Carro("Idea", "ach5q942", "Cinza", "Combustao");
+            listaDeCarros.Add(carro10);
+
+            for (int count = 0; count < listaDeCarros.Count; count++)
+            {
+                Console.WriteLine("{0}º Carro:\n{1}", count + 1, listaDeCarros[count].ToString());
+            }
+        }
+    }
+}
